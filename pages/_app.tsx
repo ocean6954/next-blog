46db176be5448f245f77902c26d0d75ff5a7a5da
ocean6954 <oceanan699@gmail.com>
2023-01-24 21:26:44 +0900

    初めてのコミット

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import 'src/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from 'components/layouts'
+import 'styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
