ccd68d68de9b5bf0caca71ed6091218236312131
ocean6954 <oceanan699@gmail.com>
2023-01-30 17:21:37 +0900

    chapter5

---
+++ components/social.js
@@ -0,0 +1,36 @@
+import styles from 'styles/social.module.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faTwitter,
+  faFacebookF,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons'
+
+export default function Social({ iconSize = 'initial', color }) {
+  return (
+    <ul
+      className={styles.list}
+      style={{ '--iconSize': iconSize, color: color }}
+    >
+      <li>
+        <a href="https://twitter.com">
+          <FontAwesomeIcon icon={faTwitter} />
+          <span className="sr-only">Twitter</span>
+        </a>
+      </li>
+      <li>
+        <a href="https://www.facebook.com">
+          <FontAwesomeIcon icon={faFacebookF} />
+          <span className="sr-only">Facebook</span>
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com">
+          <FontAwesomeIcon icon={faGithub} />
+          <span className="sr-only">GitHub</span>
+        </a>
+      </li>
+    </ul>
+  )
+}
