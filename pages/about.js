aefab7928aad13f1a0e297b69c687b190a8c5548
ocean6954 <oceanan699@gmail.com>
2023-01-31 18:33:01 +0900

    chapter6 done

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Meta from 'components/meta'
 import Hero from 'components/hero'
 import Container from 'components/container'
 import PostBody from '../components/post-body'
@@ -13,6 +14,13 @@ import eyecatch from 'images/about.jpg'
 export default function About() {
   return (
     <Container>
+      <Meta
+        pageTitle="アバウト"
+        pageDesc="About development activities"
+        pageImg={eyecatch.src}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
       <Hero title="About" subtitle="About development activities" />
       <figure>
         <Image
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,13 @@
 // const inter = Inter({ subsets: ['latin'] })
 
 import Hero from 'components/hero'
+import Meta from 'components/meta'
 import Container from 'components/container'
 
 export default function Home() {
   return (
     <Container>
+      <Meta />
       <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn />
     </Container>
   )
